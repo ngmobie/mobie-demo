2b7a81a20431fb155acdbeab598a6227ca3ede8f
Victor Queiroz <victorcqueirozg@gmail.com>
2015-05-30 23:06:54 -0300

    demo build

--- plugins/org.apache.cordova.device/www/device.js
+++ plugins/org.apache.cordova.device/www/device.js
@@ -1,4 +1,4 @@
-cordova.define("org.apache.cordova.device.device", function(require, exports, module) { /*
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -77,5 +77,3 @@ Device.prototype.getInfo = function(successCallback, errorCallback) {
 };
 
 module.exports = new Device();
-
-});
